397409a137dd87725244b6d847d62a12bdbe6ab1
Dave Teh <david.ebenezer@n2nconnect.com>
2018-10-11 17:42:17 +0800

    changed code for server.js

--- src/server.js
+++ src/server.js
@@ -1,41 +1,43 @@
+'use strict';
+
 const Hapi = require('hapi');
-const routes = require('./routes');
-const Good = require('good');
 
-const server = new Hapi.Server({
+const server = Hapi.server({
+  port: 3000,
+  host: 'localhost'
 });
-server.connection({ port: 8000, host: '0.0.0.0' });
-server.route(routes);
 
-server.register({
-  register: Good,
-  options: {
-    reporters: {
-      console: [{
-        module: 'good-squeeze',
-        name: 'Squeeze',
-        args: [{
-          response: '*',
-          log: '*',
-        }],
-      }, {
-        module: 'good-console',
-      }, 'stdout'],
-    },
-  },
-}, (err) => {
-  if (err) {
-    throw err;
+server.route({
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+
+    return 'Hello, world!';
   }
 });
 
-if (!module.parent) {
-  server.start((err) => {
-    if (err) {
-      throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`); // eslint-disable-line no-console
-  });
-}
+server.route({
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, h) => {
 
-module.exports = server;
+    return 'Hello, ' + encodeURIComponent(request.params.name) + '!';
+  }
+});
+
+const init = async () => {
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+
+
+process.on('unhandledRejection', (err) => {
+
+  console.log(err);
+  process.exit(1);
+});
+
+init();
+// module.exports = server;
