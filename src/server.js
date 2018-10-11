e454fb6d155af52b7c1b2fcea07bd5ad60057afc
Dave Teh <david.ebenezer@n2nconnect.com>
2018-10-11 17:04:27 +0800

    changed server host to ec2 public domain

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ redisClient.on('error', (error) => {
 
 const server = new Hapi.Server({
 });
-server.connection({ port: 8080, host: '0.0.0.0' });
+server.connection({ port: 8080, host: 'ec2-54-179-186-164.ap-southeast-1.compute.amazonaws.com' });
 server.route(Routes(redisClient));
 
 server.register({
