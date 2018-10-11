f130d6357714ea3775df4cdd350b84a79b6a4876
Dave Teh <david.ebenezer@n2nconnect.com>
2018-10-11 17:47:15 +0800

    added / route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,10 @@
 const ping = require('./ping');
 const urls = require('./urls');
 
-module.exports = [].concat(ping, urls);
+module.exports = [].concat(ping, urls, {
+  method: 'GET',
+  path: '/',
+  handler: (request, response) => {
+    response('hello');
+  },
+});
